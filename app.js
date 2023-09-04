ea40466a02acc3cecd88ee57a098e86cc4a162cf
zolppy <gabriel.lcifba@gmail.com>
2023-09-04 14:13:18 -0300

    add, commit e push feito por script bash

---
+++ app.js
@@ -0,0 +1,40 @@
+const daysContainer = document.querySelector('#days');
+const hoursContainer = document.querySelector('#hours');
+const minutesContainer = document.querySelector('#minutes');
+const secondsContainer = document.querySelector('#seconds');
+const nextYearContainer = document.querySelector('#year');
+const spinnerLoading = document.querySelector('#loading');
+const countdownContainer = document.querySelector('#countdown');
+
+const nextYear = new Date().getFullYear() + 1;
+const newYearTime = new Date(`January 01 ${nextYear} 00:00:00`);
+
+nextYearContainer.textContent = nextYear;
+
+const getTimeUnit = unit => unit < 10 ? '0' + unit : unit;
+
+const insertCountdownValues = ({ days, hours, minutes, seconds }) => {
+  daysContainer.textContent = getTimeUnit(days);
+  hoursContainer.textContent = getTimeUnit(hours);
+  minutesContainer.textContent = getTimeUnit(minutes);
+  secondsContainer.textContent = getTimeUnit(seconds);
+}
+
+const updateContdown = () => {
+  const currentTime = new Date();
+  const difference = newYearTime - currentTime;
+  const days = Math.floor(difference / 1000 / 60 / 60 / 24);
+  const hours = Math.floor(difference / 1000 / 60 / 60) % 24;
+  const minutes = Math.floor(difference / 1000 / 60) % 60;
+  const seconds = Math.floor(difference / 1000) % 60;
+
+  insertCountdownValues({ days, hours, minutes, seconds });
+}
+
+const handleCountdownDisplay = () => {
+  spinnerLoading.remove();
+  countdownContainer.style.display = 'flex';
+}
+
+setTimeout(handleCountdownDisplay, 1000);
+setInterval(updateContdown, 1000);
